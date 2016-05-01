faa422ad60f719c32bcc7dc02320c436c3e3c555
cernanb <cernanb@gmail.com>
2016-05-01 16:25:13 -0700

    created functions to pass jasmine tests

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(favorite){
+  return 'I love ' + favorite;
+}
+
+function shouting(phrase){
+  return phrase.toUpperCase();
+}
+
+function roundDown(num){
+  return Math.floor(num);
+}
+
+function theTruth(){
+  return true;
+}
